f536619c6a4fafdf0648f555baa4ef0ce9315913
Murillo Mamud <murillomamud@gmail.com>
2022-01-23 15:33:11 -0300

    new stream

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ client = boto3.client('firehose')
 def put_record(event:dict):
     data = json.dumps(event) + "/n" 
     response = client.put_record(
-        DeliveryStreamName='bootcamp-class2', #kinesis service name
+        DeliveryStreamName='egd-Murillo', #kinesis service name
         Record={"Data":data}
     )
-    print(event)
+    #print(event)
+    print(response)
     return response
 
 simulation = Simulation(user_pool_size=100,sessions_per_day=1000000)    
-events = simulation.run(duration_seconds=10)
+events = simulation.run(duration_seconds=5)
 
 for event in events:
     put_record(event)
